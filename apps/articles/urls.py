28e06600ce0b5b9d34f854b0f16d43f137e8ff8f
Jordi Funollet <jordi.f@ati.es>
2006-05-09 08:32:13 +0000

    Some templates added, some improved, some URLs cleaned.

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls.defaults import *
 
-info_dict = {
-    'app_label': 'articles',
-    'module_name': 'articles',
-}
+obj_dict = {'app_label': 'articles', 'module_name': 'articles', }
+section_dict = {'app_label': 'articles', 'module_name': 'sections', }
 
 
 urlpatterns = patterns('lieder.apps.misc.views',
-    (r'^$', 'limited_object_list', info_dict, ),
-    (r'^(?P<object_id>\d+)/$', 'limited_object_detail', info_dict,),
-    (r'^(?P<slug>[\w-]+)/$', 'limited_object_detail', dict(info_dict, slug_field='slug')),
+    (r'^$', 'limited_object_list', section_dict, ),
+    (r'^section/$', 'limited_object_list', section_dict,),
+    (r'^section/(?P<object_id>\d+)/$', 'limited_object_detail', section_dict,),
+    (r'^(?P<object_id>\d+)/$', 'limited_object_detail', obj_dict,),
+    (r'^(?P<slug>[\w-]+)/$', 'limited_object_detail', dict(obj_dict, slug_field='slug')),
 )
-                       
--- apps/documents/urls.py
+++ apps/documents/urls.py
@@ -6,11 +6,11 @@ categ_dict = {'app_label': 'documents', 'module_name': 'documentcategorys',}
 
 # object_id
 urlpatterns = patterns('django.views.generic.list_detail',
-    (r'^$', 'object_list', obj_dict, ),
+    (r'^$', 'object_list', categ_dict, ),
     (r'^categ/$', 'object_list', categ_dict, ),
     (r'^categ/(?P<object_id>\d+)/$', 'object_detail', categ_dict, ),
     (r'^categ/(?P<slug>[\w-]+)/$', 'object_detail', 
-    dict(categ_dict, slug_field='slug',),
+        dict(categ_dict, slug_field='slug',),
     ),
 )
                        
