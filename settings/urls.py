ba6b4ef34731cb78939fa5c5a0fcdd158f5ce869
Jordi Funollet <jordi.f@ati.es>
2006-05-17 12:34:25 +0000

    * Start page: 'concerts'.

--- settings/urls.py
+++ settings/urls.py
@@ -6,14 +6,14 @@ urlpatterns = patterns('',
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^r/', include('django.conf.urls.shortcut')),
     #
-    (r'^articles/', include('lieder.apps.articles.urls')),
+    #(r'^articles/', include('lieder.apps.articles.urls')),
     (r'^concerts/', include('lieder.apps.concerts.urls')),
     (r'^documents/', include('lieder.apps.documents.urls')),
     (r'^links/', include('lieder.apps.links.urls')),
     (r'^programmes/', include('lieder.apps.programmes.urls')),
     (r'^singers/', include('lieder.apps.singers.urls')),
     #
-    (r'^', include('lieder.apps.articles.urls')),
+    (r'^', include('lieder.apps.concerts.urls')),
 )
 
 if LOCAL_DEV:
