158ce43f4d96e4bc466a3d54d42fe876887962fa
Jordi Funollet <jordi.f@ati.es>
2006-12-28 20:11:33 +0000

    Concert's page should show just future concerts. The actual day/hour was read on urls.py, so was read only when rebooting the Django app. The time-related login has moved to the view, so it's evaluated for every request.

--- apps/concerts/urls.py
+++ apps/concerts/urls.py
@@ -3,12 +3,16 @@ from datetime import datetime
 from lieder.apps.concerts.models import Concert
 
 # Return only objects with a future 'pub_date'.
-info_dict = { 'queryset': Concert.objects.filter(pub_date__gt=str(datetime.now())) }
+info_dict = {
+    'queryset': Concert.objects.all() ,
+    'allow_empty': True,
+}
 
 info_dict_slug = dict(info_dict, slug_field='slug')
 
-urlpatterns = patterns('django.views.generic.list_detail',
-    (r'^$', 'object_list', info_dict),
-    (r'^(?P<slug>[\w-]+)/$', 'object_detail', info_dict_slug),
+urlpatterns = patterns('',
+    (r'^$', 'lieder.apps.concerts.views.futureitems_object_list', info_dict),
+    (r'^(?P<slug>[\w-]+)/$', 'django.views.generic.list_detail.object_detail', info_dict_slug),
 )
-                       
+
+
--- apps/concerts/views.py
+++ apps/concerts/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+"""Custom views for Concert objects."""
+
+from django.views.generic.list_detail import object_list
+from datetime import datetime
+
+def futureitems_object_list (request, queryset, *args, **kwargs):
+    """Calls the generic view but only shows items with pub_date in future."""
+    queryset = queryset.filter( pub_date__gt=str(datetime.now()) )
+    return object_list (request, queryset, *args, **kwargs)
